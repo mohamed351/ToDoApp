dccd278fee10756610fad4260e4d9f78149ca154
Mohamed Beshri Amer <mohamed.beshri.amer@gmail.com>
2022-08-28 23:22:32 +0200

    Make Property Change

---
+++ ToDoApp/DataService/IRestDataService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Model;
+
+namespace ToDoApp.DataService
+{
+    public interface IRestDataService
+    {
+        Task<List<ToDo>> GetToDosAsync();
+        Task AddToDoAsync(ToDo toDo);
+
+        Task UpdateToDoAsync(ToDo toDo);
+
+        Task DeleteAsync(int ID);
+
+
+    }
+}
---
+++ ToDoApp/Model/ToDo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Model
+{
+    public class ToDo : INotifyPropertyChanged
+    {
+        int _id;
+        string _taskName;
+
+        public int Id { get => _id;
+            set
+            {
+                if (_id == value)
+                    return;
+                else
+                {
+                    _id = value;
+                    this.PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(nameof(Id)));
+
+                } 
+                   
+            }
+
+
+        }
+
+        public string taskName
+        {
+            get => _taskName;
+            set
+            {
+                if (_taskName == value)
+                    return;
+                else
+                {
+                    _taskName = value;
+                    this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(taskName)));
+                }
+             
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
